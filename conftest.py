e28f51756486ca0d46364308ade02545661a4cdd
dowhilegeek <dowhilegeek@gmail.com>
2015-08-14 17:52:34 -0600

    add model fixture and fix a jank test

--- conftest.py
+++ conftest.py
@@ -1,5 +1,10 @@
 import pytest
 import json
+from authentise_services.session import Session
+from authentise_services.config import Config
+from authentise_services.model import Model
+from urllib.parse import urljoin
+
 
 import httpretty as HTTPretty
 
@@ -15,17 +20,29 @@ def httpretty():
 
 @pytest.yield_fixture
 def config():
-    from authentise_services.config import Config
-
-    yield Config()
+    c = Config()
+    c.host = "authentise.com"
+    yield c
 
 @pytest.yield_fixture
 def session(httpretty, config):
-    from authentise_services.session import Session
-
     httpretty.register_uri(httpretty.POST,
                            "https://users.{}/sessions/".format(config.host),
                            forcing_headers={"Set-Cookie": "session=1234"},
                            status=200)
 
     yield Session("herp", "derp")
+
+@pytest.yield_fixture
+def model(httpretty, config, session):
+    url = "http://derp.com/model/123/"
+
+    payload = {"status": "processed",
+               "name": "derp.stl"}
+
+    httpretty.register_uri(httpretty.GET,
+                           url,
+                           body=json.dumps(payload),
+                           status=200)
+
+    yield Model(session, url=url)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,9 @@
 from authentise_services.config import Config
 import tempfile
 
-def test_config_no_config_file_happy_path():
-    config = Config()
-    assert config.host == "authentise.com"
-
 
 def test_magic_dir_method():
     config = Config()
-    assert config.host == "authentise.com"
     assert dir(config) == ['host', 'password', 'username']
 
 
