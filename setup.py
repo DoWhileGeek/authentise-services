8785ab0f18429276f25a9c8349510105a90927b6
dowhilegeek <dowhilegeek@gmail.com>
2015-08-15 17:59:37 -0600

    forgot to call version writer

--- setup.py
+++ setup.py
@@ -42,27 +42,28 @@ def write_version():
 
 
 def main():
-    setup(
-        name="authentise_services",
-        url="https://github.com/DoWhileGeek/authentise-services",
-        description="A client library for Authentise open services",
-        author="Joeseph Rodrigues",
-        author_email="dowhilegeek@gmail.com",
-        version=get_version(),
-        packages=["authentise_services"],
-        package_data={"authentise_services": ["authentise_services/*"], },
-        include_package_data=True,
-        install_requires=[
-            "requests==2.7.0",
-        ],
-        extras_require={
-            'develop': [
-                'pylint==1.4.3',
-                'pytest==2.6.4',
-                'httpretty==0.8.10',
-                'twine==1.5.0',
-            ]},
-    )
+    with write_version():
+        setup(
+            name="authentise_services",
+            url="https://github.com/DoWhileGeek/authentise-services",
+            description="A client library for Authentise open services",
+            author="Joeseph Rodrigues",
+            author_email="dowhilegeek@gmail.com",
+            version=get_version(),
+            packages=["authentise_services"],
+            package_data={"authentise_services": ["authentise_services/*"], },
+            include_package_data=True,
+            install_requires=[
+                "requests==2.7.0",
+            ],
+            extras_require={
+                'develop': [
+                    'pylint==1.4.3',
+                    'pytest==2.6.4',
+                    'httpretty==0.8.10',
+                    'twine==1.5.0',
+                ]},
+        )
 
 
 if __name__ == "__main__":
